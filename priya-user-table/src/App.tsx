c5873c5a51e876d376a378dabd6f95d36971abf0
Priyatomar-56 <priyatomar@iitbhilai.ac.in>
2025-03-11 22:06:45 +0530

    my project with all the inital changes

---
+++ priya-user-table/src/App.tsx
@@ -0,0 +1,11 @@
+import UserTable from "../src/UserTable";
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+      <UserTable />
+    </div>
+  );
+}
+
+export default App;
